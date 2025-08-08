b22307326aa28b4225f1f489d8a072833c03dd2d
Piruthiviraj08 <piruthiviraj0f8@gmail.com>
2025-08-08 14:46:17 +0530

    fix: updated vite config for SPA routing 3

--- orphanage-website/vite.config.js
+++ orphanage-website/vite.config.js
@@ -5,12 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   base: '/',
-  server: {
-    historyApiFallback: true, // <-- This is key for dev
-  },
-  build: {
-    outDir: 'dist',
-  },
 })
